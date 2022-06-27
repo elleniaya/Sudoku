f869abe30f92e3abe023296e9a315a8f6de0d51a
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-06-28 00:53:37 +0700

    Create RecordsPanel.java

---
+++ view/RecordsPanel.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class RecordsPanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics g) {
+        
+    }
+}
