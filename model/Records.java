6d3391fb70d4e06b39644de863b9941002fafa26
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-06-28 00:46:19 +0700

    Create Records.java

---
+++ model/Records.java
@@ -0,0 +1,95 @@
+package model;
+
+import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Records {
+    private String[] columnsHeader = new String[] {"Name", "Points"};
+    private static int NOTUSED = -1;
+
+    ArrayList<Pair> data = new ArrayList<>();
+    String[][] dataTable;
+
+    public Records() {
+        readRecords();
+    }
+
+    private void readRecords() {
+        try{
+            String fileName = "records.txt";
+            Path path = Paths.get(fileName);
+            Scanner scanner = new Scanner(path);
+            scanner.useDelimiter(System.getProperty("line.separator"));
+            while(scanner.hasNextLine()){
+                data.add(parseLine(scanner.nextLine()));
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Pair parseLine(String line) {
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter(" ");
+        String name = scanner.next();
+        String points = scanner.next();
+        scanner.close();
+        return new Pair(name, Integer.parseInt(points));
+    }
+
+    public void writeRecords() {
+        try {
+            FileWriter writer = new FileWriter("records.txt");
+            for (Pair pair : data) {
+                writer.write(pair.name + " " + pair.points + System.getProperty("line.separator"));
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addRecord(String name, int glasses) {
+        if (nameUsed(name) != NOTUSED) {
+            data.get(nameUsed(name)).updatePoints(glasses);
+        } else {
+            data.add(new Pair(name, glasses));
+        }
+        sortTable();
+    }
+
+    private int nameUsed(String name) {
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).name.equals(name)) {
+                return i;
+            }
+        }
+        return NOTUSED;
+    }
+
+    public String[] getcolumnsHeader() {
+        return columnsHeader;
+    }
+
+    public String[][] getDataTable() {
+        sortTable();
+        updateDataTable();
+        return dataTable;
+    }
+
+    private void updateDataTable() {
+        for (int i = 0; i < data.size(); i++) {
+            dataTable[i][1] = data.get(i).name;
+            dataTable[i][2] = Integer.toString(data.get(i).points);
+        }
+    }
+
+    private void sortTable() {
+        Collections.sort(data, Comparator.comparingInt(Pair::getPoints));
+        Collections.reverse(data);
+    }
+}
