6f6ed0142370a948e5bdac5e468b4c482f698dec
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-06-28 05:12:08 +0700

    Create Listener.java

---
+++ src/view/Listener.java
@@ -0,0 +1,8 @@
+package view;
+
+public interface Listener {
+    void exitTheGame();
+    void clickNumberButton(String value, int row, int column);
+    void passbuttonListener();
+    void recordsButton();
+}
