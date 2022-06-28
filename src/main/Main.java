cc4a7a4497d8426e4f19accc63bacb095aabe177
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-06-28 16:17:50 +0700

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,12 @@
+package main;
+
 import controller.SudokuController; 
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        //GameSettings settings = new GameSettings();
         SudokuController controller = new SudokuController();
         controller.run();
     }
