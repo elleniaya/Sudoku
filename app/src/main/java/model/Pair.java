66fd681bc9328e51ec8ff9c611862bdcc8b75e35
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-07-01 12:37:33 +0700

    Update Pair.java

--- app/src/main/java/model/Pair.java
+++ app/src/main/java/model/Pair.java
@@ -1,16 +1,12 @@
 package model;
 
-public class Pair implements Comparable<Pair> {
+public class Pair {
     public String name;
     public int points;
 
     public Pair(String name, int points) {
         this.name = name;
         this.points = points;
-    }
-
-    public int compareTo(Pair o1) {
-        return name.compareTo(o1.name);
     }
 
     public int getPoints() {
